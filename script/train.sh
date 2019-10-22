4c68295a6c9b44be4e7c6ef831730e83209a6438
amax <1240917537@qq.com>
2019-10-22 04:08:55 +0000

    train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,10 +1,15 @@
-k=5
+k=10
+mkdir ../output/model_textcnn
 mkdir ../output/model_textcnn/fold_$k
 cd ../src
-for((i=0;i<k;i++));  
+for((i=7;i<k;i++));  
 do   
 python3 main.py \
--server-ip='10.15.82.238' \
+-server-ip='10.15.82.239' \
+-predict=1 \
+-training=1 \
+-port=8190 \
+-port-out=5556 \
 -model=../output/model_textcnn/fold_$k/model_$i.pkl \
 -train-file=fold_$k/data_$i/train.csv \
 -dev-file=fold_$k/data_$i/dev.csv \
